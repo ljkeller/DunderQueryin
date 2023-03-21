c20de8926933b9368fdadb33931da8b7d2ca869e
Lucas Keller <kellerjlucas@gmail.com>
2023-03-20 19:01:07 -0500

    Statically server dunder_ai.html
    Code changes to serve dunder_ai.html statically.
    
    Now to style & add to dunder_ai.html.

--- DunderAI/DunderAI/settings.py
+++ DunderAI/DunderAI/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'DunderQueryin'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'DunderAI.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'DunderQueryin'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- DunderAI/DunderQueryin/views.py
+++ DunderAI/DunderQueryin/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def dunder_ai(request):
+    return render(request, 'dunder_ai.html', {})
 
 # Create your views here.
