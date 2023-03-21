de07d73ad73aac8400568cd682421adb5f8b6d22
Lucas Keller <kellerjlucas@gmail.com>
2023-03-20 19:38:01 -0500

    Serving dunder_ai HTML/CSS
    Changes allow serving styled HTML through static/ . This requires some
    changes to settings.py that are tracked in https://docs.djangoproject.com/en/4.1/intro/tutorial06/.
    
    TODO: link form with JS or Django. Process POST request. Send
    information back about model prediction.

--- DunderAI/DunderQueryin/views.py
+++ DunderAI/DunderQueryin/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def dunder_ai(request):
-    return render(request, 'dunder_ai.html', {})
+    return render(request, 'DunderQueryin/dunder_ai.html', {})
 
 # Create your views here.
