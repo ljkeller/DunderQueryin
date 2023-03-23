ee51c3541e86be412161deddb69ac701bfae5f83
Lucas Keller <kellerjlucas@gmail.com>
2023-03-23 07:29:46 -0500

    Handle user input request end to end
    Several things are part of this commit:
    1. Added torch model prediction into views.py... Current implementation
       expect a model stored in data/model.
    2. Stored best model in data/model
    3. Send model prediction (class and percentage) to dunder_ai template
    4. Render model prediction with image, name, and percentage
    
    In summary, a user can now put a quote into the quote box, submit the
    quote, and be given the models results in a pretty format.

--- DunderAI/DunderQueryin/views.py
+++ DunderAI/DunderQueryin/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from django.urls import reverse
+
+from fastai.text.all import *
+
+# TODO: Should we serve this through static/DunderQueryin?
+MODEL_PATH='data/model'
+
+def quote_to_prediction(quote):
+    learner = load_learner(MODEL_PATH)
+    character, character_tensor, tensors = learner.predict(quote)
+    return character, tensors[character_tensor].item()
+
+def build_render_context(character, probability):
+    model_results_context = {}
+    model_results_context['character_prediction'] = character
+    model_results_context['prediction_probability'] = "{:.1f}".format(probability*100)
+    return model_results_context
 
 def dunder_ai(request):
     return render(request, 'DunderQueryin/dunder_ai.html', {})
@@ -11,9 +25,11 @@ def query(request):
     except (KeyError):
         return render(request, 'DunderQueryin/dunder_ai.html', {'error_message': 'Something went wrong with your quote!'})
     else:
+        # TODO: Enforce 'normal' quote input
         print(quote)
-        # Predict here
+
+        character_prediction, prediction_probability = quote_to_prediction(quote)
 
         # Not sending HTPP response, but I should be
         # workaround to render correctly on this webpage
-        return render(request, 'DunderQueryin/dunder_ai.html', {'model_prediction': 'Kevin'})
+        return render(request, 'DunderQueryin/dunder_ai.html', build_render_context(character_prediction, prediction_probability))
